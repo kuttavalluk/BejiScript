e91caaf3d143dae90f3caa1171aa54a8a3a62902
kuttavalluk <79351044+kuttavalluk@users.noreply.github.com>
2021-02-20 13:32:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import os
 
 #atelmarkascript
 if os.name == 'nt':
-    os.system("cd IFB && python print.py")
+    os.system("cd BejiScript && cd IFB && python print.py")
 if os.name == 'posix':
-    os.system("cd IFB && python3 print.py")
+    os.system("cd BejiScript && cd IFB && python3 print.py")
 
 if os.name == 'nt':
-    os.system("cd IFB && python dictattack.py")
+    os.system("cd BejiScript && cd IFB && python dictattack.py")
 if os.name == 'posix':
-    os.system("cd IFB && python3 dictattack.py")
+    os.system("cd BejiScript && cd IFB && python3 dictattack.py")
